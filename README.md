e28dc31766648b4c40c0aae82cea2648c25477e3
frikki898 <frikki898@gmail.com>
2020-08-31 14:28:03 +0000

    fixy

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Splits mp3 audiobooks into chapters by detecting long silent pauses in the mp3 file
 
 ## Prerequisites
-Python and ffmpeg must be installed and added to path
+Python and ffmpeg must be installed
+windows users must also have these programs added to path
 
 ## Useage example
 ```
-python chapterSplitter.py 3 -50
+python chapterSplitter.py nameOfAudio.mp3 3 -50
 ```
 In this example the program will look for 3 second gaps that have -50db or lower and split on those sections
 In my experience these values do a good job finding all chapter splits but occasionally some chapters are cut into the same file. Feel free to try messing around with the parameters to find values that works for you
--- chapterSplitter.py
+++ chapterSplitter.py
@@ -2,17 +2,11 @@ import sys
 import subprocess
 
 filename = ""
-silenceDuration = 3
+silenceDuration = 4
 db = -50
+
 if(len(sys.argv) < 2):
-    print("first parameter must be name of file to split")
-    print("example 'python bookname.mp3'")
-if(len(sys.argv) < 3):
-    print("using default parameter values")
-    print("""
-silenceDuration = 3
-db = -50
-""")
+    print("error")
 else:
     filename = sys.argv[1]
     silenceDuration = int(sys.argv[2])
@@ -25,21 +19,13 @@ startDuration = 0
 duration = 0
 i = 1
 f = open("output.txt", "r")
-bookname = filename.split(".mp3")[0]
 for line in f:
     if("silence_start: " in line):
         splitline = line.split("silence_start: ")
         duration = float(splitline[1]) - startDuration
-        subprocess.check_output('ffmpeg -ss ' + str(startDuration) + ' -i hod.mp3 -t ' + str(duration) + ' -c copy ' + bookname + '_chap' + str(i) + '.mp3', shell=True)
+        subprocess.check_output('ffmpeg -ss ' + str(startDuration) + ' -i hod.mp3 -t ' + str(duration) + ' -c copy chap' + str(i) + '.mp3', shell=True)
         print("CREATED CHAPTER" + str(i))
         i += 1
     elif("silence_end: " in line):
         splitline = line.split("silence_end: ")
         startDuration = float(splitline[1].split(" | ")[0])
-
-
-
-
-
-
-
